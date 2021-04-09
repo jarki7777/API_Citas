7fcec5003a4b6f4f8be6b8bdf0620b650c884552
TheJarki <melendez.jarki@gmail.com>
2021-04-09 22:11:26 +0200

    login endpoint
    /auth/login

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,20 +1,20 @@
 import jwt from 'jsonwebtoken';
-import Users from '../models/users.model.js';
+import { Users } from '../models/users.js';
 
-export const jwtController = {
+export const authController = {
     authenticate: async (req, res) => {
         try {
             const data = {
                 email: req.headers.email,
                 password: req.headers.password
             }
-            const checkUser = await Users.findOne({ email: data.email });
+            const checkUser = await Users.findOne( { where: { email: data.email }});
             if (checkUser && checkUser.email === data.email && checkUser.password === data.password) {                
                 const jwtPayload = {
                     email: req.headers.email
                 }                
                 const token = jwt.sign(jwtPayload, process.env.SECRET);
-                res.status(200).json({ token, id: checkUser._id });
+                res.status(200).json({ token, id: checkUser.id });
             } else {
                 res.status(404).send({ message: 'the email-password combination does not exist'});
             }
