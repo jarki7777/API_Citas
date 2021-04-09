044debb61b7ed65d0c01fb2ed7b859464f90e0a9
TheJarki <melendez.jarki@gmail.com>
2021-04-09 23:01:13 +0200

    jwt middleware for dashboard

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ import dotenv from 'dotenv';
 import userRoutes from './routes/user.routes.js';
 import authRoutes from './routes/auth.routes.js';
 
-
 dotenv.config();
 
 const app = express();
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,13 +1,11 @@
 import { Router } from 'express';
 import { userController } from '../controllers/user.controller.js';
 import { checkJwt } from '../middleware/checkJwt.js';
-import { checkRole } from '../middleware/checkRole.js';
-
 
 const userRoutes = Router();
 
 userRoutes.post('/signup', userController.create);
 
-userRoutes.get('/dashboard', userController.dashboard);
+userRoutes.get('/dashboard', checkJwt, userController.dashboard);
 
 export default userRoutes;
