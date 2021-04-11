2a9c1e2d772b1b6b536ad478970f96894340e145
Israel Sanchez <50373290+IsrraelS@users.noreply.github.com>
2021-04-11 20:37:33 +0200

    Update 20210409165922-Appointments.cjs

--- seeders/20210409165922-Appointments.cjs
+++ seeders/20210409165922-Appointments.cjs
@@ -10,7 +10,7 @@ module.exports = {
           date: '2021/04/15',
           time: '14:00:00',
           status:'pending',
-          usersId:2,
+          usersId:1,
           doctorsId:2,
           createdAt: '2021/04/01',
           updatedAt: '2021/04/01'
